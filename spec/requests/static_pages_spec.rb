6a059424bb416862d9d5445e7d7326789def02e0
KLY <yaydeepthought@gmail.com>
2012-06-19 17:07:21 +0800

    route to register page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,13 @@
 #encoding=utf-8
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "静态页面" do
   describe "首页" do
     subject { page }
     before { visit root_path }
-    it { should have_content('首页')}
+    it { should have_selector('title',text: "KLY的Rails练习") }
+    it { should have_content('主页') }
   end
+  
 end
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,7 +1,10 @@
+#encoding=utf-8
 require 'spec_helper'
 
 describe "UserPages" do
-  describe "" do
-     #sdfds
+  describe "注册新用户" do
+      subject { page }
+      before { visit new_user_path }
+      it { should have_selector('title',text: '注册新用户') }
   end
 end

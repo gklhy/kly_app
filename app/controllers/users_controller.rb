e0f36858a896b6c84696fb08c7c5e5f0e83cce7a
KLY <yaydeepthought@gmail.com>
2012-07-11 16:50:24 +0800

    signin module finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save 
+      #如果保存成功，切换到登录后的界面
+      sign_in @user
       #如果保存成功，转到欢迎界面
       flash[:success] = "注册成功！欢迎来到KLY的Rails世界！"
       redirect_to @user #等价于 link_to "user", user_path

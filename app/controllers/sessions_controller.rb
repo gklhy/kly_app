b9b9ffaedf1085397bbe0a80a8c25a284da667c6
KLY <yaydeepthought@gmail.com>
2012-07-11 13:53:09 +0800

    add session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,23 @@
+#encoding=utf-8
 class SessionsController < ApplicationController
+   def create
+   	  #render 'new'
+   	  user = User.find_by_email(params[:session][:email])
+      if user && user.authenticate(params[:session][:password])
+        #goto profile page
+        sign_in user
+        redirect_to user 
+      else
+        #goto sigin page again
+        # flash[:error] = "用户名或者密码不正确！" #出现之后就去不掉了，需要用 flash.now只出现一次
+        flash.now[:error] = "用户名或者密码不正确！"
+        render 'new'
+      end
+   end
    def new 
    end
-   def create
-   end
    def destroy
+   end
+   def index
    end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,7 +7,7 @@ describe "验证" do
       before { visit signin_path }
       it { should have_selector('title', text: '登录') }
       it { should have_selector('h1', text: '登录') }
-      it { should have_selector('div.alert.alert-error',text: '出错啦！')}
+      it { should have_selector('div.alert.alert-error',text: '用户名或者密码不正确！')}
 
       describe "输入正确的登录信息" do
          let(:user) { Factory.create(:user) }
@@ -20,6 +20,10 @@ describe "验证" do
          it { should have_link('个人信息', href: user_path(:user)) }
          it { should have_link('退出', href: signout_path) }
          it { should_not have_link('登录', href: signin_path) }
+      end
+      describe "跳转到其他的页面" do
+         before { click_link "主页"}
+         it { should_not have_selector('div.alert.alert-error')}
       end
    end
 
